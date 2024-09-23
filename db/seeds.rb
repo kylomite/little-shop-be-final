19d6547274f3f4a6603073c32b1d0c83932e1dc6
Kyle Delaney <khdelaney99@gmail.com>
2024-09-23 11:57:47 -0400

    populate seeds data

--- db/seeds.rb
+++ db/seeds.rb
@@ -14,4 +14,9 @@ system(cmd)
 
 system("rails db:migrate")
 
-Coupon.create(name: "test", code: "bogo", value_off: 15, percent_off: true, active: true, merchant_id: 1)
+Coupon.create(name: "Summer Sale", code: "Bingo", value_off: 15, percent_off: true, active: true, merchant_id: 1)
+Coupon.create(name: "Sweepstakes", code: "marple", value_off: 30, percent_off: true, active: false, merchant_id: 2)
+Coupon.create(name: "Fall Sale", code: "2407", value_off: 65, percent_off: false, active: true, merchant_id: 1)
+Coupon.create(name: "Winter Sale", code: "1738 Hey Whats up", value_off: 25, percent_off: true, active: true, merchant_id: 3)
+Coupon.create(name: "Spring Sale", code: "Full coweling", value_off: 15, percent_off: false, active: false, merchant_id: 4)
+Coupon.create(name: "Birthday Special", code: "Gear 5", value_off: 50, percent_off: true, active: true, merchant_id: 1)
--- spec/factories/coupon.rb
+++ spec/factories/coupon.rb
@@ -1,10 +1,10 @@
 FactoryBot.define do
-    factory :customer do
+    factory :coupon do
       name { Faker::Name.first_name }
       code { Faker::Name.last_name }
       value_off { Faker::Number.number(digits:2)}
       percent_off { Faker::Boolean.boolean}
       active { Faker::Boolean.boolean}
-      merchant_id { Faker::Number.number(digits:4)}
+      merchant_id { Faker::Number.number(digits:3)}
     end
   end
